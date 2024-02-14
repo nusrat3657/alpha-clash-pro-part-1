f1d3bb78c884c2de56ac51a5fcf96deddeb316c8
nusrat3657 <nusratjahan3657r@gmail.com>
2024-02-15 02:45:29 +0600

    completed the website

--- scripts/utility.js
+++ scripts/utility.js
@@ -18,12 +18,31 @@ function removeBackgroundColorById(elementId){
     element.classList.remove('bg-orange-400');
 }
 
+
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 function getARandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
 
     const randomNumber = Math.random()*25;
-    const index = Math.round(randomNumber);
+    const index = Math.round(randomNumber); //round holo purnoshongkhay rupantor kore
 
     const alphabet = alphabets[index];
     return alphabet;
